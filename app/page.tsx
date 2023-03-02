ffefc86abf7a5001436cabeb7182c9986be16fdc
yangfengyi <sheepfly@yeah.net>
2023-03-02 23:06:28 +0800

    feat: remove unuseful fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import QuestionForm from "@/components/QuestionForm";
 
 export default async function Home() {
-  const result = await fetch("http://localhost:3000/api/hello").then((res) => {
-    return res.text();
-  });
   return (
     <main>
       <div className="text-black flex flex-col w-full py-20">
